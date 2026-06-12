101c724834f6968bd8a07c761ff0e3f9a60c3ed2
Oshan Bajracharya <oshanbaj11@gmail.com>
2026-06-12 15:19:55 +0545

    react-router-dom  (NavLink) from hit api useEffect hook

--- JavaScript/React/vite-project/src/CosmosApp.jsx
+++ JavaScript/React/vite-project/src/CosmosApp.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import LearnUseState from './LearnUseState'
+import CosmosLink from './CosmosLink'
 
 const CosmosApp = () => {
   return (
     <div>
-        <LearnUseState></LearnUseState>
+        {/* <LearnUseState></LearnUseState> */}
+        <CosmosLink></CosmosLink>
+
     </div>
 
   )
--- JavaScript/React/vite-project/src/main.jsx
+++ JavaScript/React/vite-project/src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import "./global.css"
 import CosmosApp from './CosmosApp.jsx'
+import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 {/*     <App /> */}
-<CosmosApp>
-  
-</CosmosApp>
+<BrowserRouter>
+<CosmosApp> </CosmosApp>
+</BrowserRouter>
+
   </StrictMode>,
 )
